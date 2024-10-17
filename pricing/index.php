07fe87cb877de8ba2f6caa92bb8ceb158f769c6a
vvtek-net <kenngx9x@gmail.com>
2024-10-17 15:45:35 +0700

    update 3:45 pm - 17.10.2024

---
+++ pricing/index.php
@@ -0,0 +1,155 @@
+
+<!DOCTYPE HTML>
+<html lang="zxx">
+
+<head>
+	<title>Business Pricing Tables Responsive Widget Template :: w3layouts</title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="Business Pricing Tables Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- Meta tag Keywords -->
+
+	<!-- css file -->
+	<link rel="stylesheet" href="pricing/css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<!-- //css file -->
+
+	<!-- web-fonts -->
+	<link href="//fonts.googleapis.com/css?family=Encode+Sans+Condensed:100,200,300,400,500,600,700,800,900&amp;subset=latin-ext,vietnamese"
+	 rel="stylesheet">
+	<!-- //web-fonts -->
+</head>
+
+<body>
+	<div class="main-bg">
+		
+		<!-- content -->
+		<div class="lm-tabel">
+			<div class="lm-item lm-item-2">
+				<div class="lm-item-top">
+					<span class="lm-item-title lm-underline">Cơ Bản</span>
+					<div class="lm-item-price">4,188,000
+						<span>VNĐ</span>
+					</div>
+
+				</div>
+				<div class="lm-item-body">
+
+					<ul class="lm-item-list">
+						<li>
+							<strong>25</strong> Trang Bố Cục</li>
+						<li>
+							<strong>50GB</strong> Băng Thông </li>
+						<li>
+							Thiết Kế Chuẩn <strong>SEO</strong></li>
+						<li>
+							<strong>Bảo Hành</strong> Trọn Đời</li>
+						<li>
+							<strong>Miến Phí</strong> Hosting + Tên Miền</li>
+						<li>
+							<strong>Miến Phí</strong> Bảo Mật SSL</li>
+					</ul>
+					<div class="buy-button">
+						<a class="popup-with-zoom-anim" href="#popup1">Đăng Ký Ngay</a>
+					</div>
+				</div>
+				<div class="lm-item-bottom"></div>
+			</div>
+			<div class="lm-item lm-item-1">
+				<div class="lm-item-top">
+					<span class="lm-item-title lm-underline">Tiêu Chuẩn</span>
+					<div class="lm-item-price item-price2-w3ls">6,188,000
+						<span>VNĐ</span>
+					</div>
+				</div>
+				<div class="lm-item-body">
+
+					<ul class="lm-item-list">
+						<li>
+							<strong>50</strong> Trang Bố Cục</li>
+						<li>
+							<strong>100GB</strong> Băng Thông </li>
+						<li>
+							Thiết Kế Chuẩn <strong>SEO</strong></li>
+						<li>
+							<strong>Bảo Hành</strong> Trọn Đời</li>
+						<li>
+							<strong>Miến Phí</strong> Hosting + Tên Miền</li>
+						<li>
+							<strong>Miến Phí</strong> Bảo Mật SSL</li>
+					</ul>
+					<div class="buy-button">
+						<a class="popup-with-zoom-anim" href="#popup1">Đăng Ký Ngay</a>
+					</div>
+				</div>
+
+				<div class="lm-item-bottom"></div>
+			</div>
+			<div class="lm-item lm-item-3">
+				<div class="lm-item-top">
+					<span class="lm-item-title lm-underline">Cao Cấp</span>
+					<div class="lm-item-price item-price3-w3ls">9,988,000
+						<span>VNĐ</span>
+					</div>
+				</div>
+				<div class="lm-item-body">
+
+					<ul class="lm-item-list">
+						<li>
+							<strong>150</strong> Trang Bố Cục</li>
+						<li>
+							<strong>Không Giới Hạn</strong> Băng Thông </li>
+						<li>
+							Thiết Kế Chuẩn <strong>SEO</strong></li>
+						<li>
+							<strong>Bảo Hành</strong> Trọn Đời</li>
+						<li>
+							<strong>Miến Phí</strong> Hosting + Tên Miền</li>
+						<li>
+							<strong>Miến Phí</strong> Bảo Mật SSL</li>
+					</ul>
+					<div class="buy-button">
+						<a class="popup-with-zoom-anim" href="#popup1">Đăng Ký Ngay</a>
+					</div>
+				</div>
+				<div class="lm-item-bottom"></div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<!-- //content -->
+
+		<!-- popup -->
+		<div id="popup1" class="pop-overlay">
+			<div class="popup">
+				<div class="signin-form profile">
+					<h3>Register Here</h3>
+					<div class="login-form">
+						<form action="#" method="post">
+							<input type="text" name="name" placeholder="Username" required="">
+							<input type="email" name="email" placeholder="E-mail" required="">
+							<input type="text" name="Number" placeholder="Contact Number" required="">
+							<input type="password" name="password" placeholder="Password" id="password1" required="">
+							<input type="password" name="password" placeholder="Confirm Password" id="password2" required="">
+							<input type="submit" value="Submit">
+						</form>
+					</div>
+					<a class="close" href="#">&times;</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+</body>
+
+</html>
